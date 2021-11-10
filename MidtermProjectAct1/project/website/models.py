f1f9de2fc0240db74aeea32e0f90603b6e23b658
coookie1010 <xavierking3014@gmail.com>
2021-11-10 22:32:44 +0800

    feat(server): additional website backend data
    Prepare for sql statements information that needed.
    Import some new database tables.
    Clean up.

--- MidtermProjectAct1/project/website/models.py
+++ MidtermProjectAct1/project/website/models.py
@@ -12,7 +12,8 @@ class Note(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
     notes = db.relationship('Note')
