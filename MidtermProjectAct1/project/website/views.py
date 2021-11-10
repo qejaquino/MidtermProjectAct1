7ddcb227128645006344b46b8f8d316b12698ab4
coookie1010 <xavierking3014@gmail.com>
2021-11-11 01:26:19 +0800

    Accounts and some updates.

--- MidtermProjectAct1/project/website/views.py
+++ MidtermProjectAct1/project/website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, User
 from . import db
 import json
 
@@ -8,20 +8,15 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
+
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
-
     return render_template("home.html", user=current_user)
+
+@views.route('/account')
+@login_required
+def account():
+    all_data = User.query.all()
+    return render_template("account.html", user=current_user, users=all_data)
 
 @views.route('/about')
 def about():
